4860d5ede97ed9deafb1204b441e3dd964935398
AbdullahSaeed007 <gmsiddiquie@gmail.com>
2024-11-18 19:48:29 +0500

    reverted the cors policy

--- backend/App.js
+++ backend/App.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://13.61.107.145",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // allow cookies
   })
